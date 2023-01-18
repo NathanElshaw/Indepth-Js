d769ab8d9c0cde8b8c8627118b23b2a54e78a747
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-18 18:59:28 -0500

    worked on Library Project

--- Assignments/Library/core.js
+++ Assignments/Library/core.js
@@ -1,27 +1,39 @@
-let myLibrary = [];
 const btn = document.querySelector("#addBook");
 const title = document.querySelector("#title").value;
 const author = document.querySelector("#author").value;
 const pages = document.querySelector("#pages").value;
+const books = document.querySelector("#book");
+const p = document.createElement("p");
+let myLibrary = [];
+let addNewBook = "";
+
+class Book {
+  addBook(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+
+    addNewBook = `${title}, By: ${author}, ${pages} pages`;
+  }
+}
+
+class newBook extends Book {
+  addBookToLibrary(book) {
+    myLibrary = [book];
+  }
+}
+
+function appendClass() {
+  myLibrary.forEach((book) => (books.appendChild(p).textContent = book));
+}
 
 btn.addEventListener("click", (event) => {
-  console.log(title, author, pages);
   if (title && author && pages !== null) {
-    var newBook = new Book(title, author, pages);
-    newBook.addBookToLibrary(book);
+    event.preventDefault();
+    const currentBook = new newBook();
+    currentBook.addBook(title, author, pages);
+    currentBook.addBookToLibrary(addNewBook);
+    appendClass();
   }
-  alert("error");
   event.preventDefault();
 });
-
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  let book = title + "by: " + author + " " + pages + "pages";
-}
-
-function addBookToLibrary(book) {
-  this.myLibrary = [book];
-  console.log(myLibrary);
-}
