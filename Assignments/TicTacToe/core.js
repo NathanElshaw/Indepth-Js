6410ecb17ffcb2de7a0114d39a284d42cf3bb775
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-21 20:08:47 -0500

    Add most of Frontend

--- Assignments/TicTacToe/core.js
+++ Assignments/TicTacToe/core.js
@@ -1,12 +1,34 @@
 const gameBox = document.querySelectorAll("#gameBox");
+const markX = document.querySelector("#markX");
+const markO = document.querySelector("#markO");
+
+let mark = "X";
+let arr = [];
+markX.addEventListener("click", (event) => {
+  markX.classList.add("assigned");
+  markO.classList.remove("assigned");
+  mark = "X";
+  event.preventDefault();
+});
+markO.addEventListener("click", (event) => {
+  markO.classList.add("assigned");
+  markX.classList.remove("assigned");
+  mark = "O";
+  event.preventDefault();
+});
+
+const Player = () => {};
 
 gameBox.forEach((box) => {
-  console.log(box);
   box.addEventListener("click", () => {
-    if (box.textContent != null) {
-      box.textContent = "X";
-    } else {
-      console.log("filled already");
-    }
+    arr.push(box);
+    box.textContent = mark;
+    checkWin();
   });
 });
+
+function checkWin() {
+  if ((arr[(0, 2)].textContent = "O")) {
+    alert("game over");
+  }
+}
