368cbcac8ed90af9f5f9adb6355cb66e54a0793b
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-29 18:30:25 -0500

    Finished About pages and website

--- Assignments/Restaurant-Page/Src/index.js
+++ Assignments/Restaurant-Page/Src/index.js
@@ -1,15 +1,13 @@
 import "./styles.css";
 import menuPage from "./MenuPage";
 import homePage from "./HomePage";
+import aboutPage from "./AboutPage";
 
 const btnHome = document.querySelector("#home");
 const btnMenu = document.querySelector("#menu");
 const btnAbout = document.querySelector("#about");
 
-function aboutPage() {
-  content.innerHTML = "";
-  content.innerHTML = "About";
-}
+homePage();
 
 btnHome.addEventListener("click", () => {
   homePage();
