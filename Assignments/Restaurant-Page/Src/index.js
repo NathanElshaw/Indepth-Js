40d7ce33ce162a1f7d320670e9cdddf99d297d5d
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-29 15:16:58 -0500

    Started on homepage html/js

--- Assignments/Restaurant-Page/Src/index.js
+++ Assignments/Restaurant-Page/Src/index.js
@@ -7,10 +7,16 @@ const content = document.querySelector("#content");
 
 function homePage() {
   content.innerHTML = "";
-  content.textContent = "Home";
+  content.innerHTML = `<h2>Welcome to our Pizzaria!</h2>
+  <p>We create the best pizzas in our area hands down!</p>
+  <p>Voted the best Pizzaria in town for the past 5 years!</p>
+  <img src="./Src/hero.jpg" alt="" id="heroImg" />
+  <p>Come pay us a visit or order online!</p>
+  <div id="cta">
+    <button id="ctaDirection">Direction</button>
+    <button id="ctaOrder">Order Online</button>
+  </div>`;
 }
-
-homePage();
 
 function menuPage() {
   content.innerHTML = "";
