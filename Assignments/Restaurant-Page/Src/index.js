2abcc9b962e4f88deeca86549025ac8200673fbc
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-29 18:10:49 -0500

    added text to menu

--- Assignments/Restaurant-Page/Src/index.js
+++ Assignments/Restaurant-Page/Src/index.js
@@ -1,72 +1,14 @@
 import "./styles.css";
+import menuPage from "./MenuPage";
+import homePage from "./HomePage";
 
 const btnHome = document.querySelector("#home");
 const btnMenu = document.querySelector("#menu");
 const btnAbout = document.querySelector("#about");
-const content = document.querySelector("#content");
-
-function homePage() {
-  content.innerHTML = "";
-  content.innerHTML = `
-  <h2>Welcome to our Pizzaria!</h2>
-  <p>We create the best pizzas in our area hands down!</p>
-  <p>Voted the best Pizzaria in town for the past 5 years!</p>
-  <img src="../Src/hero.jpg" alt="" id="heroImg" />
-  <p>Come pay us a visit or order online!</p>
-  <div id="cta">
-    <button id="ctaDirection">Direction</button>
-    <button id="ctaOrder">Order Online</button>
-  </div>`;
-}
-
-homePage();
-
-function menuPage() {
-  content.innerHTML = "";
-  content.innerHTML = `<h2>Menu</h2>
-  <div class="menuGrid">
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-    <div class="pizza">
-      <img src="../Src/pizza.svg" alt="" />
-      <h3>Type</h3>
-      <p>disc</p>
-      <button>Order Now!</button>
-    </div>
-  </div>`;
-}
 
 function aboutPage() {
   content.innerHTML = "";
-  content.textContent = "About";
+  content.innerHTML = "About";
 }
 
 btnHome.addEventListener("click", () => {
