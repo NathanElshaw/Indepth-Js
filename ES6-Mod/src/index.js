c42fcf3eeab10655886760541369d8999ae3c1df
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-29 09:32:15 -0500

    Added Restuarant Assignemnt

--- ES6-Mod/src/index.js
+++ ES6-Mod/src/index.js
@@ -1,9 +1,15 @@
 import myName from "./myName";
+import "./styles.css";
+import Icon from "./Icon.svg";
 
 function component() {
   const element = document.createElement("div");
 
   element.textContent = myName("Nathan");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
   return element;
 }
 
