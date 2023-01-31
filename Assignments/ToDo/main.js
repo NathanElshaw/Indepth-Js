be1c4f005fda408c0c1c87171e69faa09f421d39
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-01-31 18:33:07 -0500

    Added function factories to Tasks and Projects

--- Assignments/ToDo/main.js
+++ Assignments/ToDo/main.js
@@ -1,21 +1,37 @@
 const displayTask = document.querySelector("#displayTasks");
 const taskTitle = document.querySelector("#taskTitle");
 const taskDesc = document.querySelector("#taskDesc");
+const taskType = document.querySelector("#type");
 const addTask = document.querySelector("#addTask");
 
-const tasks = (title, desc) => {
+let displayList = [];
+let id = 0;
+
+const tasks = (title, desc, type, id) => {
   const proto = {
-    type: "task",
     title: title,
     desc: desc,
+    type: type,
+    id: id,
   };
   return Object.assign(Object.create(proto));
 };
 
 const displayTasks = (s) => {
-  return console.log(s.desc);
+  displayTask.innerHTML = "";
+  displayList.push(s);
+  displayList.forEach((list) => {
+    if (Object.getPrototypeOf(list).type === "task") {
+      displayTask.innerHTML += `
+      <h2>${list.title}</h2>
+      <p>${list.desc}</p
+      <button id=${list.id}>Delete</button>
+      `;
+    }
+  });
 };
 
 addTask.addEventListener("click", () => {
-  console.log(displayTasks(tasks(taskTitle.value, taskDesc.value)));
+  displayTasks(tasks(taskTitle.value, taskDesc.value, taskType.value, id));
+  id++;
 });
