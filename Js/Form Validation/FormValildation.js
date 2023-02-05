56c0e124c390d3266f3fffb5a4f295f30092f197
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-05 14:05:40 -0500

    Notes on form validition

--- Js/Form Validation/FormValildation.js
+++ Js/Form Validation/FormValildation.js
@@ -1,15 +1,14 @@
-const username = document.querySelector("#username");
-const email = document.querySelector("#email");
+const email = document.getElementById("#email");
 const submit = document.querySelector("#submit");
-
-submit.addEventListener("click", (event) => {
-  event.preventDefault();
-});
 
 email.addEventListener("input", (event) => {
   if (email.validity.typeMismatch) {
-    email.setCustomValidity("It needs to be longer");
+    email.setCustomValidity("I am expecting an email address!");
   } else {
     email.setCustomValidity("");
   }
+});
+
+submit.addEventListener("click", (event) => {
+  event.preventDefault();
 });
