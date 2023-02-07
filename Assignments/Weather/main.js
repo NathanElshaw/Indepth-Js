332e3f676d95c94881aae7d39d81baa2eb1e980a
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-06 19:56:21 -0500

    added displaying some data to DOM

--- Assignments/Weather/main.js
+++ Assignments/Weather/main.js
@@ -1,23 +1,24 @@
 import KEY from "./key.js";
 
 window.onload = function () {
-  async function getLocation() {
-    navigator.geolocation.getCurrentPosition(function (pos) {
-      getWeather(pos.coords.latitude, pos.coords.longitude);
-    });
-  }
+  //   async function getLocation() {
+  //     navigator.geolocation.getCurrentPosition(function (pos) {
+  //       getWeather(pos.coords.latitude, pos.coords.longitude);
+  //     });
+  //   }
   async function getWeather(lat, lon) {
     try {
       const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${KEY}`,
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${KEY}`,
         { method: "GET", mode: "cors" }
       );
       const weatherData = await data.json();
       console.log(weatherData);
+      document.body.innerHTML = `Temp: ${weatherData.main.temp} Feels Like: ${weatherData.main.feels_like}`;
     } catch (e) {
       console.log(e);
     }
   }
-
-  getLocation();
+  getWeather(44.59, -89.123);
+  //   getLocation();
 };
