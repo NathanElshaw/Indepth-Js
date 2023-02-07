35dc2cdd10a8cd9ec6d5d5bf650476507ac31670
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-06 20:19:53 -0500

    added a basic search bar

--- Assignments/Weather/main.js
+++ Assignments/Weather/main.js
@@ -1,4 +1,33 @@
 import KEY from "./key.js";
+const city = document.querySelector("#cityId");
+const getCity = document.querySelector("#weatherSearch");
+const weatherInfo = document.querySelector("#weatherInfo");
+
+async function getWeather(val1, val2) {
+  let data = "";
+  console.log(typeof val1);
+  if (typeof val1 === "string") {
+    try {
+      data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${val1}&units=imperial&appid=${KEY}`
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    try {
+      data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${val1}&lon=${val2}&units=imperial&appid=${KEY}`,
+        { method: "GET", mode: "cors" }
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  const weatherData = await data.json();
+  console.log(weatherData);
+  weatherInfo.innerHTML = `Name: ${weatherData.name} Temp: ${weatherData.main.temp} Feels Like: ${weatherData.main.feels_like}`;
+}
 
 window.onload = function () {
   //   async function getLocation() {
@@ -6,19 +35,12 @@ window.onload = function () {
   //       getWeather(pos.coords.latitude, pos.coords.longitude);
   //     });
   //   }
-  async function getWeather(lat, lon) {
-    try {
-      const data = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${KEY}`,
-        { method: "GET", mode: "cors" }
-      );
-      const weatherData = await data.json();
-      console.log(weatherData);
-      document.body.innerHTML = `Temp: ${weatherData.main.temp} Feels Like: ${weatherData.main.feels_like}`;
-    } catch (e) {
-      console.log(e);
-    }
-  }
+
+  getCity.addEventListener("click", (event) => {
+    event.preventDefault();
+    getWeather(city.value);
+  });
+
   getWeather(44.59, -89.123);
   //   getLocation();
 };
