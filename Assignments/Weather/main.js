132d76db1cfc617ef1684306dde2633a54915ddb
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-08 11:49:02 -0500

    added 5 day forcast

--- Assignments/Weather/main.js
+++ Assignments/Weather/main.js
@@ -2,6 +2,7 @@ import KEY from "./key.js";
 const city = document.querySelector("#cityId");
 const getCity = document.querySelector("#weatherSearch");
 const weatherInfo = document.querySelector("#weatherInfo");
+const forecastInfo = document.querySelector("#forecastInfo");
 
 async function getWeather(val1, val2) {
   let data = "";
@@ -27,7 +28,7 @@ async function getWeather(val1, val2) {
         { method: "GET", mode: "cors" }
       );
       forecast = await fetch(
-        (forecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${val1}&lon=${val2}&appid=${KEY}`),
+        (forecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${val1}&lon=${val2}&units=imperial&appid=${KEY}`),
         { method: "GET", mode: "cors" }
       );
     } catch (e) {
@@ -36,15 +37,32 @@ async function getWeather(val1, val2) {
   }
   const weatherData = await data.json();
   const forecastData = await forecast.json();
-  console.log(forecastData);
+
   weatherInfo.innerHTML = `
   <h2>${weatherData.name}</h2>
   <p>${weatherData.weather[0].main} <img src="http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"</p>
   <p>Temp: ${weatherData.main.temp}</p>
   <p>H: ${weatherData.main.temp_max} L: ${weatherData.main.temp_min}</p>
   <p>Feels like:    ${weatherData.main.feels_like}</p>`;
-}
 
+  let forecastArr = [
+    forecastData.list[0],
+    forecastData.list[7],
+    forecastData.list[15],
+    forecastData.list[23],
+    forecastData.list[31],
+  ];
+  forecastArr.forEach((forecast) => {
+    forecastInfo.innerHTML += `
+    <div>
+    <p>Date: </p> 
+   <h2>${forecast.weather[0].main}</h2>
+    <p>Temp:</p>
+    <p>H: ${forecast.main.temp_max} L: ${forecast.main.temp_min}</p>
+    <p>Feels like:</p>
+    </div>`;
+  });
+} //0,7,15,23,31
 window.onload = function () {
   //   async function getLocation() {
   //     navigator.geolocation.getCurrentPosition(function (pos) {
