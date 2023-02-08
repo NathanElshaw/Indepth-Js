6a9bed2b8e22aa7f06c71ffb0747f78353b3cc03
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-08 12:25:27 -0500

    added mix max functions for temps

--- Assignments/Weather/main.js
+++ Assignments/Weather/main.js
@@ -46,19 +46,86 @@ async function getWeather(val1, val2) {
   <p>Feels like:    ${weatherData.main.feels_like}</p>`;
 
   let forecastArr = [
-    forecastData.list[0],
-    forecastData.list[7],
-    forecastData.list[15],
-    forecastData.list[23],
-    forecastData.list[31],
+    (forecast = [
+      forecastData.list[0],
+      forecastData.list[1],
+      forecastData.list[2],
+      forecastData.list[3],
+      forecastData.list[4],
+      forecastData.list[5],
+      forecastData.list[6],
+    ]),
+    (forecast = [
+      forecastData.list[7],
+      forecastData.list[8],
+      forecastData.list[9],
+      forecastData.list[10],
+      forecastData.list[11],
+      forecastData.list[12],
+      forecastData.list[13],
+      forecastData.list[14],
+    ]),
+    (forecast = [
+      forecastData.list[15],
+      forecastData.list[16],
+      forecastData.list[17],
+      forecastData.list[18],
+      forecastData.list[19],
+      forecastData.list[20],
+      forecastData.list[21],
+      forecastData.list[22],
+    ]),
+    (forecast = [
+      forecastData.list[23],
+      forecastData.list[24],
+      forecastData.list[25],
+      forecastData.list[26],
+      forecastData.list[27],
+      forecastData.list[28],
+      forecastData.list[29],
+      forecastData.list[30],
+    ]),
+    (forecast = [
+      forecastData.list[31],
+      forecastData.list[32],
+      forecastData.list[33],
+      forecastData.list[34],
+      forecastData.list[35],
+      forecastData.list[36],
+      forecastData.list[37],
+      forecastData.list[38],
+    ]),
   ];
+
+  function min(arg) {
+    let num = 1000;
+    for (let i = 0; i < arg.length; i++) {
+      if (arg[i].main.temp_min < num) {
+        num = arg[i].main.temp_min;
+      }
+    }
+    console.log(num);
+    return num;
+  }
+
+  function max(arg) {
+    let num = 0;
+    for (let i = 0; i < arg.length; i++) {
+      if (arg[i].main.temp_min > num) {
+        num = arg[i].main.temp_min;
+      }
+    }
+    console.log(num);
+    return num;
+  }
+
   forecastArr.forEach((forecast) => {
+    console.log(forecastArr);
     forecastInfo.innerHTML += `
     <div>
-    <p>Date: </p> 
-   <h2>${forecast.weather[0].main}</h2>
-    <p>Temp:</p>
-    <p>H: ${forecast.main.temp_max} L: ${forecast.main.temp_min}</p>
+    <p>Date: ${forecast[0].dt_txt}</p> 
+   <h2>${forecast[0].weather[0].main}</h2>
+    <p>H: ${max(forecast)} L: ${min(forecast)}</p>
     <p>Feels like:</p>
     </div>`;
   });
