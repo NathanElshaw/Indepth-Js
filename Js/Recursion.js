2770974543796616417490509d549a9aa441e032
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-09 19:51:58 -0500

    Recursion learning

--- Js/Recursion.js
+++ Js/Recursion.js
@@ -8,7 +8,7 @@ function pow(x, n) {
 }
 
 //Pratice
-
+//For loop
 function sumAll(n) {
   let a = n;
   let b = 0;
@@ -18,13 +18,28 @@ function sumAll(n) {
   }
   return n;
 }
-
+//Recursion
 function sumAll2(n) {
-  if ((n = 1)) {
+  if (n == 1) {
     return n;
   } else {
     return n + sumAll2(n - 1);
   }
 }
+//Arithmatic progression
+function sumAll3(n) {
+  return (n * (1 + n)) / 2;
+}
 
-console.log(sumAll2(100));
+//Factorials
+//Recursion
+function factorial(n) {
+  return n != 1 ? n * factorial(n - 1) : 1;
+}
+
+//Fibonacci
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+
+console.log(fib(2));
