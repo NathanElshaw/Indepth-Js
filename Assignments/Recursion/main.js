58c11e6c837898aca600ac6c3c148565946bea5a
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-11 18:02:18 -0500

    One way of doing merge sort

--- Assignments/Recursion/main.js
+++ Assignments/Recursion/main.js
@@ -4,18 +4,44 @@ function fib(n) {
   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
 
-let merArr = [5, 2, 1, 3, 6, 4];
+const mergeSortRec = (arr) => {
+  if (arr.length === 0) return "Please provide a non empty array!";
+  if (arr.length === 1) return arr;
 
-function mergeSort(n) {
-  let arrLength = n.length;
-  let newArr = [];
-  let middle = arrLength / 2;
-  let left = n.slice(0, middle);
-  let right = n.slice(middle, arrLength);
-  if (arrLength < 2) {
-    return n;
-  } else {
+  const mid = Math.floor(arr.length / 2);
+  const left = arr.slice(0, mid);
+  const right = arr.slice(mid, arr.length);
+
+  return merge(mergeSortRec(left), mergeSortRec(right));
+};
+
+const merge = (leftArr, rightArr) => {
+  //To merge the both incoming arrays
+  const result = [];
+
+  let iL = 0;
+  let iR = 0;
+
+  while (iL < leftArr.length && iR < rightArr.length) {
+    if (leftArr[iL] < rightArr[iR]) {
+      result.push(leftArr[iL]);
+      iL++;
+    } else {
+      result.push(rightArr[iR]);
+      iR++;
+    }
   }
-}
 
-console.log(mergeSort(merArr));
+  while (iL < leftArr.length) {
+    result.push(leftArr[iL]);
+    iL++;
+  }
+
+  while (iR < rightArr.length) {
+    result.push(rightArr[iR]);
+    iR++;
+  }
+  return result;
+};
+
+console.log(mergeSortRec([5, 2, 1, 3, 6, 4]));
