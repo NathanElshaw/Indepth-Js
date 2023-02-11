033cf6eaab93adea212c14ec765dc1b763d3ab56
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-11 00:20:03 -0500

    started on merge sort -- Able to break down left and right

--- Assignments/Recursion/main.js
+++ Assignments/Recursion/main.js
@@ -5,3 +5,20 @@ function fib(n) {
 }
 
 let merArr = [5, 2, 1, 3, 6, 4];
+
+function mergeSort(n) {
+  console.log(n);
+  let arrLength = n.length;
+  if (arrLength < 2) {
+    return n;
+  } else {
+    let newArr = [];
+    let middle = arrLength / 2;
+    let left = n.slice(0, middle);
+    let right = n.slice(middle, arrLength);
+    console.log(left, right);
+    return newArr.push(mergeSort(right) < mergeSort(left));
+  }
+}
+
+console.log(mergeSort(merArr));
