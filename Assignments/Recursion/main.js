ac0809546919479b383b55aa79e2c8dcb499d4dc
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-11 22:30:48 -0500

    Merge sort added

--- Assignments/Recursion/main.js
+++ Assignments/Recursion/main.js
@@ -4,44 +4,39 @@ function fib(n) {
   return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
 
-const mergeSortRec = (arr) => {
-  if (arr.length === 0) return "Please provide a non empty array!";
-  if (arr.length === 1) return arr;
-
-  const mid = Math.floor(arr.length / 2);
-  const left = arr.slice(0, mid);
-  const right = arr.slice(mid, arr.length);
-
-  return merge(mergeSortRec(left), mergeSortRec(right));
+const mergeSort = (arr) => {
+  if (arr.length < 2) {
+    return arr;
+  } else {
+    let mid = Math.floor(arr.length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid, arr.length);
+    return Merge(mergeSort(left), mergeSort(right));
+  }
 };
 
-const merge = (leftArr, rightArr) => {
-  //To merge the both incoming arrays
-  const result = [];
-
-  let iL = 0;
-  let iR = 0;
-
-  while (iL < leftArr.length && iR < rightArr.length) {
-    if (leftArr[iL] < rightArr[iR]) {
-      result.push(leftArr[iL]);
-      iL++;
+const Merge = (leftArr, rightArr) => {
+  let result = [];
+  while (leftArr.length > 0 && 0 < rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      result.push(leftArr[0]);
+      leftArr.splice(0, 1);
     } else {
-      result.push(rightArr[iR]);
-      iR++;
+      result.push(rightArr[0]);
+      rightArr.splice(0, 1);
     }
   }
-
-  while (iL < leftArr.length) {
-    result.push(leftArr[iL]);
-    iL++;
+  if (leftArr.length > 0) {
+    leftArr.forEach((arr) => {
+      result.push(arr);
+    });
   }
-
-  while (iR < rightArr.length) {
-    result.push(rightArr[iR]);
-    iR++;
+  if (rightArr.length > 0) {
+    rightArr.forEach((arr) => {
+      result.push(arr);
+    });
   }
   return result;
 };
 
-console.log(mergeSortRec([5, 2, 1, 3, 6, 4]));
+console.log(mergeSort([5, 2, 1, 3, 6, 4]));
