8c2ce8f5c31b57c4fc16c53520e39d9ec4f58162
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-12 13:11:19 -0500

    skeleton of linked list functions

---
+++ Assignments/LinkedList/linkedList.js
@@ -0,0 +1,25 @@
+const linkList = (arr) => {
+  const proto = {
+    addNode: (arg) => {},
+
+    append: (arg) => {},
+
+    preappend: (arg) => {},
+
+    size: (arg) => {},
+
+    tail: (arg) => {},
+
+    at: (arg) => {},
+
+    pop: (arg) => {},
+
+    contains: (arg) => {},
+
+    find: (arg) => {},
+
+    toString: (arg) => {},
+  };
+  Object.assign.Object(proto);
+};
+const node = (data) => {};
--- Assignments/Recursion/main.js
+++ Assignments/Recursion/main.js
@@ -39,4 +39,9 @@ const Merge = (leftArr, rightArr) => {
   return result;
 };
 
-console.log(mergeSort([5, 2, 1, 3, 6, 4]));
+console.log(
+  mergeSort([
+    5, 2, 1, 3, 6, 4, 43, 42, 13, 64, 875, 1351, 5552, 265, 2564, 4552, 32,
+    2342, 56, 23, 434, 56, 66, 54, 45, 434, 43, 454, 2424, 2,
+  ])
+);
