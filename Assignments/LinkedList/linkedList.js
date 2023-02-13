bd0b1bd6b00895db974aa6f061caaac454c03540
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-12 20:17:06 -0500

    added link list rough

--- Assignments/LinkedList/linkedList.js
+++ Assignments/LinkedList/linkedList.js
@@ -1,44 +1,153 @@
-const linkList = () => {
+const Node = (value) => {
+  return {
+    value: value || null,
+    nextNode: null,
+    appendValue(input) {
+      this.value = input;
+    },
+  };
+};
+
+const LinkedList = () => {
   let HEAD = null;
   let length = 0;
 
   const append = (value) => {
-    const NewNode = Node(value);
+    const newNode = Node(value);
     length++;
-    if (HEAD == null) {
-      return (HEAD = NewNode);
+    if (HEAD === null) {
+      return (HEAD = newNode);
     }
     let pointer = HEAD;
     while (pointer.nextNode !== null) {
       pointer = pointer.nextNode;
     }
-    pointer.nextNode = NewNode;
+    pointer.nextNode = newNode;
   };
 
-  const preappend = (value) => {
+  const prepend = (value) => {
     const newNode = Node(value);
     length++;
-    if (HEAD !== null) {
+    if (HEAD === null) {
       return (HEAD = newNode);
     }
     newNode.nextNode = HEAD;
     HEAD = newNode;
   };
 
+  const size = () => {
+    if (!HEAD) return null;
+    return length;
+  };
+
+  const head = () => {
+    if (!HEAD) return null;
+    return HEAD.value;
+  };
+
+  const tail = () => {
+    if (!HEAD) return null;
+    let pointer = HEAD;
+    while (pointer.nextNode !== null) {
+      pointer = pointer.nextNode;
+    }
+    return pointer.value;
+  };
+
+  const at = (index) => {
+    if (!HEAD) return null;
+    let pointer = HEAD;
+    for (let i = 0; i < index; i++) {
+      pointer = pointer.nextNode;
+    }
+    return pointer;
+  };
+
+  const pop = () => {
+    if (!HEAD) return null;
+    // for a List with one node
+    if (HEAD.nextNode === null) {
+      HEAD = null;
+    }
+    // find the pointer before the tail
+    at(size() - 2).nextNode = null;
+    length--;
+  };
+
+  const contains = (value) => {
+    if (!HEAD) return null;
+    let pointer = HEAD;
+    while (pointer.nextNode !== null) {
+      if (pointer.value === value) return true;
+      pointer = pointer.nextNode;
+    }
+    // check the tail node as well
+    return pointer.value === value ? true : false;
+  };
+
+  const find = (value) => {
+    if (!HEAD) return null;
+    let index = 0;
+    let pointer = HEAD;
+    while (pointer.nextNode !== null) {
+      if (pointer.value === value) return index;
+      pointer = pointer.nextNode;
+      index++;
+    }
+    // check the tail node as well
+    return pointer.value === value ? index : null;
+  };
+
+  const toString = () => {
+    if (!HEAD) return null;
+    let pointer = HEAD;
+    let result = "";
+    while (pointer.nextNode !== null) {
+      result = result.concat(`( ${pointer.value} ) --> `);
+      pointer = pointer.nextNode;
+    }
+    // for the tail
+    result = result.concat(`( ${pointer.value} ) --> null`);
+    console.log(result);
+    return result;
+  };
+
+  const insertAt = (value, index) => {
+    if (!HEAD) return null;
+    const newNode = Node(value);
+    length++;
+    let pointer = HEAD;
+    for (let i = 0; i < index - 1; i++) {
+      pointer = pointer.nextNode;
+    }
+    const nextNode = pointer.nextNode;
+    pointer.nextNode = newNode;
+    newNode.nextNode = nextNode;
+  };
+
+  const removeAt = (index) => {
+    if (!HEAD) return null;
+    if (index > length || index < 0) return;
+    if (index === 0) HEAD = HEAD.nextNode;
+    else {
+      const prePointer = at(index - 1);
+      prePointer.nextNode = prePointer.nextNode.nextNode;
+    }
+    length--;
+  };
+
   return {
     append,
-    preappend,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+    insertAt,
+    removeAt,
   };
 };
-
-const Node = (input) => {
-  return {
-    value: input || null,
-    nextNode: null,
-    appendValue(newInput) {
-      this.value = newInput;
-    },
-  };
-};
-
-console.log(Node.appendValue("new"));
