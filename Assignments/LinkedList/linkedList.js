c48a3e9e3195d22dc4be69d2bd1d628b7184c722
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-13 17:38:36 -0500

    start own linked list

--- Assignments/LinkedList/linkedList.js
+++ Assignments/LinkedList/linkedList.js
@@ -1,153 +1,33 @@
-const Node = (value) => {
-  return {
-    value: value || null,
-    nextNode: null,
-    appendValue(input) {
-      this.value = input;
-    },
-  };
-};
+class LinkedList {
+  constructor() {
+    (this.HEAD = null), (this.length = 0);
+  }
 
-const LinkedList = () => {
-  let HEAD = null;
-  let length = 0;
-
-  const append = (value) => {
-    const newNode = Node(value);
-    length++;
-    if (HEAD === null) {
-      return (HEAD = newNode);
+  append(data) {
+    const newNode = new Node(data, null);
+    while (this.next != null) {
+      this.next = newNode;
     }
-    let pointer = HEAD;
-    while (pointer.nextNode !== null) {
-      pointer = pointer.nextNode;
-    }
-    pointer.nextNode = newNode;
-  };
+    this.length++;
+  }
 
-  const prepend = (value) => {
-    const newNode = Node(value);
-    length++;
-    if (HEAD === null) {
-      return (HEAD = newNode);
-    }
-    newNode.nextNode = HEAD;
-    HEAD = newNode;
-  };
+  insertAtHead(data) {
+    const newNode = new Node(data, this.HEAD);
+    this.HEAD = newNode;
+    this.length++;
+  }
+}
 
-  const size = () => {
-    if (!HEAD) return null;
-    return length;
-  };
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
-  const head = () => {
-    if (!HEAD) return null;
-    return HEAD.value;
-  };
+const ll = new LinkedList();
 
-  const tail = () => {
-    if (!HEAD) return null;
-    let pointer = HEAD;
-    while (pointer.nextNode !== null) {
-      pointer = pointer.nextNode;
-    }
-    return pointer.value;
-  };
+ll.insertAtHead(10);
+ll.append(15);
 
-  const at = (index) => {
-    if (!HEAD) return null;
-    let pointer = HEAD;
-    for (let i = 0; i < index; i++) {
-      pointer = pointer.nextNode;
-    }
-    return pointer;
-  };
-
-  const pop = () => {
-    if (!HEAD) return null;
-    // for a List with one node
-    if (HEAD.nextNode === null) {
-      HEAD = null;
-    }
-    // find the pointer before the tail
-    at(size() - 2).nextNode = null;
-    length--;
-  };
-
-  const contains = (value) => {
-    if (!HEAD) return null;
-    let pointer = HEAD;
-    while (pointer.nextNode !== null) {
-      if (pointer.value === value) return true;
-      pointer = pointer.nextNode;
-    }
-    // check the tail node as well
-    return pointer.value === value ? true : false;
-  };
-
-  const find = (value) => {
-    if (!HEAD) return null;
-    let index = 0;
-    let pointer = HEAD;
-    while (pointer.nextNode !== null) {
-      if (pointer.value === value) return index;
-      pointer = pointer.nextNode;
-      index++;
-    }
-    // check the tail node as well
-    return pointer.value === value ? index : null;
-  };
-
-  const toString = () => {
-    if (!HEAD) return null;
-    let pointer = HEAD;
-    let result = "";
-    while (pointer.nextNode !== null) {
-      result = result.concat(`( ${pointer.value} ) --> `);
-      pointer = pointer.nextNode;
-    }
-    // for the tail
-    result = result.concat(`( ${pointer.value} ) --> null`);
-    console.log(result);
-    return result;
-  };
-
-  const insertAt = (value, index) => {
-    if (!HEAD) return null;
-    const newNode = Node(value);
-    length++;
-    let pointer = HEAD;
-    for (let i = 0; i < index - 1; i++) {
-      pointer = pointer.nextNode;
-    }
-    const nextNode = pointer.nextNode;
-    pointer.nextNode = newNode;
-    newNode.nextNode = nextNode;
-  };
-
-  const removeAt = (index) => {
-    if (!HEAD) return null;
-    if (index > length || index < 0) return;
-    if (index === 0) HEAD = HEAD.nextNode;
-    else {
-      const prePointer = at(index - 1);
-      prePointer.nextNode = prePointer.nextNode.nextNode;
-    }
-    length--;
-  };
-
-  return {
-    append,
-    prepend,
-    size,
-    head,
-    tail,
-    at,
-    pop,
-    contains,
-    find,
-    toString,
-    insertAt,
-    removeAt,
-  };
-};
+console.log(ll);
