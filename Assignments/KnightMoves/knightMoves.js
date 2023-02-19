3e4ef6cc73627279a45629a8db562b201a2c3b00
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-19 15:55:30 -0500

    more to recursive search

--- Assignments/KnightMoves/knightMoves.js
+++ Assignments/KnightMoves/knightMoves.js
@@ -4,7 +4,6 @@ i.e KnightMoves([0,0], [1,2]) Output:: [0,0] -> [1,2] 1 Move
 Prevent from going out side the board
 
 */
-
 const KnightOffset = [
   [1, 2],
   [1, -2],
@@ -17,42 +16,42 @@ const KnightOffset = [
 ];
 
 class KnightMoves {
-  constructor(current, end) {
-    this.current = current;
-    this.end = end;
-  }
-
   getPosMoves(current, end, moves) {
     let isFound = false;
     let queue = [];
     if (moves === undefined) moves = [];
-    KnightOffset.forEach((kMoves) => {
-      let x = current[0] + kMoves[0];
-      let y = current[1] + kMoves[1];
-      if (x > 8 || x < 1 || y > 8 || y < 1) {
-        return null;
-      } else {
-        queue.push([x, y]);
-      }
-    });
-    moves++;
-    for (let i = 0; i < queue.length; i++) {
-      let current = queue[i];
-      if (current[0] === end[0] && current[1] === end[1]) {
-        isFound = true;
-      }
-    }
-    if (isFound === true) {
-      return {
-        current: `[${current}] -> [${end}]`,
-        moves: moves,
-      };
-    } else {
-      console.log(queue);
-      queue.forEach((q) => {
-        return this.getPosMoves(q, end, moves);
+    if (moves > 7) {
+      KnightOffset.forEach((kMoves) => {
+        let x = current[0] + kMoves[0];
+        let y = current[1] + kMoves[1];
+        if (x > 8 || x < 1 || y > 8 || y < 1) {
+          return null;
+        } else {
+          queue.push([x, y]);
+        }
       });
+      moves++;
+      for (let i = 0; i < queue.length; i++) {
+        let qR = queue[i];
+        if (qR[0] === end[0] && qR[1] === end[1]) {
+          isFound = true;
+        }
+      }
+      if (isFound === true) {
+        return {
+          current: `[${current}] -> [${end}]`,
+          moves: moves,
+        };
+      } else if (isFound === false) {
+        console.log(queue);
+        queue.forEach((q) => {
+          return this.getPosMoves(q, end, moves);
+        });
+      } else {
+        return;
+      }
     }
+    return queue;
   }
 }
 
