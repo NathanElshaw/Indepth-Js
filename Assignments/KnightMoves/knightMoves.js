efcb10331bce7f310a044fa06398cb92c31540bc
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-19 12:09:37 -0500

    started to work on logic

--- Assignments/KnightMoves/knightMoves.js
+++ Assignments/KnightMoves/knightMoves.js
@@ -6,9 +6,6 @@ Prevent from going out side the board
 */
 
 const KnightMoves = (start, finish) => {
-  let [posX, posY] = [start[0], start[1]];
-  let [endX, endY] = [finish[0], finish[1]];
-
   const knightsOffset = [
     [2, -1],
     [2, 1],
@@ -19,6 +16,35 @@ const KnightMoves = (start, finish) => {
     [-2, 1],
     [-2, -1],
   ];
+
+  const Knight = new Gameboard(start);
+  Knight.calc(start, finish);
+  return Knight;
 };
 
-KnightMoves([1, 3]);
+class Gameboard {
+  constructor(head) {
+    this.root = head;
+  }
+
+  calc(start, finish) {
+    let [posX, posY] = [start[0], start[1]];
+    let [endX, endY] = [finish[0], finish[1]];
+  }
+}
+
+class possibleMove {
+  constructor(current, opt1, opt2, opt3, opt4, opt5, opt6, opt7, opt8) {
+    current: current;
+    opt1: null;
+    opt2: null;
+    opt3: null;
+    opt4: null;
+    opt5: null;
+    opt6: null;
+    opt7: null;
+    opt8: null;
+  }
+}
+
+console.log(KnightMoves([1, 3], [3, 3]));
