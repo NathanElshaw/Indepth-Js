28d752d2677206d93a93bfc284e6deed9f20072a
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-21 17:06:23 -0500

    Added reverse string

--- Assignments/Testing/main.js
+++ Assignments/Testing/main.js
@@ -6,15 +6,44 @@ module.exports = {
     return first;
   },
 
-  reverseStr(input, n) {
-    n === undefined ? (n = 0) : (n = n);
-    if (n == input.length - 1) {
-      return input;
+  reverseStr(input) {
+    let result = "";
+    let n = input.length;
+    for (let i = 0; i < n; i++) {
+      result = result.concat(input.substr(-1, 1));
+      input = input.slice(0, input.length - 1);
     }
-    let last = input.substr(-1, 1);
-    last = last.concat(input.slice(0, input.length - 1));
-    console.log(last);
-    n++;
-    return this.reverseStr(last, n);
+    return result;
+  },
+
+  ceaserCiper(input, offset) {
+    const Alph = [
+      a,
+      b,
+      c,
+      d,
+      e,
+      f,
+      g,
+      h,
+      i,
+      j,
+      k,
+      l,
+      m,
+      n,
+      o,
+      p,
+      q,
+      r,
+      s,
+      t,
+      u,
+      v,
+      w,
+      x,
+      y,
+      z,
+    ];
   },
 };
