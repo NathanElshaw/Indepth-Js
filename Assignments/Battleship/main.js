07e9c842391aaa9dcef2d3cdbf96197ac69a0be5
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-23 17:20:51 -0500

    started on turn class

--- Assignments/Battleship/main.js
+++ Assignments/Battleship/main.js
@@ -70,4 +70,31 @@ class gameBoard {
   }
 }
 
+class turns {
+  constructor(player) {
+    this.turnPlayer = player || null;
+  }
+
+  init(player) {
+    this.turnPlayer == null ? (this.turnPlayer = player) : {};
+  }
+
+  turn(placement, player1, player2) {
+    if (this.fire(placement) == "hit") {
+      this.promptTurn(player1);
+    } else {
+      this.promptTurn(player2);
+    }
+  }
+
+  fire(placement, player2) {
+    if (gameBoard.gameBoard[placement] == "ship") {
+      this.checkSink();
+      return "hit";
+    } else {
+      return "miss";
+    }
+  }
+}
+
 module.exports = gameBoard;
