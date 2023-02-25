9b6f82140285d6efab6ea36f306d09d5fb6dd352
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-24 23:41:45 -0500

    added more player function

--- Assignments/Battleship/turns.js
+++ Assignments/Battleship/turns.js
@@ -1,27 +1,30 @@
 class turns {
   constructor(player, player2) {
+    this.currentPlayer = null;
     this.player1 = player || null;
-    this.player2 = player2 || null
+    this.player2 = player2 || null;
   }
 
-  init(player) {
-    this.turnPlayer == null ? (this.turnPlayer = player) : {};
+  init(player1, player2) {
+    this.turnPlayer == null ? (this.player = player) : null;
   }
 
-  initAi(){
-    aiActive == true ? {
-      this.player2 = "ai"
-    }
+  initAi(aiActive) {
+    aiActive == true ? (this.player2 = "ai") : null;
+  }
+
+  promptTurn(player) {
+    return `Its ${player}'s turn`;
   }
 
   turn(placement, player1, player2) {
+    player1 == "ai" ? this.aiMove() : (player1 = player1);
     if (this.fire(placement) == "hit") {
       this.promptTurn(player1);
     } else {
       this.promptTurn(player2);
     }
   }
-
 }
 
 module.exports = turns;
