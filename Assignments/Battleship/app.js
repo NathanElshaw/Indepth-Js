0a05780deb3cfb0e22f169ca4af6e86e721c61fd
NathanElshaw <NathanElshaw@users.noreply.github.com>
2023-02-27 20:35:13 -0500

    start over

--- Assignments/Battleship/app.js
+++ Assignments/Battleship/app.js
@@ -1,28 +1,12 @@
-import comp from "./comp.js";
-import gameBoard from "./gameBoard.js";
-import ships from "./ships.js";
+import board from "./gameBoard.js";
 import turns from "./turns.js";
+import comp from "./comp.js";
+import ships from "./ships.js";
 
-const ai = new comp();
-const board = new gameBoard();
-const ship = new ships();
-const gamePlayers = new turns("Player1", "Ai");
+const initGame = document.querySelector("#initGame");
 
-const player = document.querySelector("#player1Board");
-const aiBoard = document.querySelector("#aiBoard");
-console.log(gamePlayers.player1, gamePlayers.player2);
-board.createBoard();
-
-board.gameBoard.forEach((tile) => {
-  player.innerHTML += `<button class="tile">  </button>`;
-});
-
-board.aiGameboard.forEach((tile) => {
-  aiBoard.innerHTML += `<button class="tile" id="aiTile" value=${board.aiGameboard.indexOf(
-    tile
-  )} onClick=${gamePlayers.turn(
-    board.aiGameboard.indexOf(tile),
-    gamePlayers.player1,
-    gamePlayers.player2
-  )};>  </button>`;
+const game = new board();
+initGame.addEventListener("click", () => {
+  game.createBoard();
+  console.log(game);
 });
